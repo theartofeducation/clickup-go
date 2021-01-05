c732245bc59b26cefd08abf6da440416e9d605ac
Christopher Lamm <chris@theantichris.com>
2021-01-05 08:00:24 -0500

    refactor: added Event type

--- webhook.go
+++ webhook.go
@@ -1,8 +1,16 @@
 package clickup
 
+// Event is the action that triggered the Webhook.
+type Event string
+
+// Webhook Events.
+const (
+	EventTaskStatusUpdated = "taskStatusUpdated"
+)
+
 // Webhook holds the information for a ClickUp Webhook.
 type Webhook struct {
 	ID     string `json:"webhook_id"`
-	Event  string
+	Event  Event
 	TaskID string `json:"task_id"`
 }
